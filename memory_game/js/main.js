ce24f9cb77c6a6b986e8503da018b6213c895d4d
Charity Holt <charity@Charitys-MacBook-Pro.local>
2019-04-23 16:19:33 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,29 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king"; 
-var cardFour = "king";
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
-console.log("User flipped" + cardThree);
-console.log("User flipped" + cardFour);
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+var flipCard = function (cardId) {
+console.log("User flipped" + cards[cardId]);
+cardsInPlay.push([cardId]);
+if (cardsInPlay.length === 2) {
+		checkForMatch()
+	}else{
+		return(cardId);
+	}
+};
+var checkForMatch = function(){
+if (cardsInPlay[0] === cardsInPlay[1]){
+	console.log("You found a match!");
+}
+else {
+	console.log("Sorry, try again!");
+		}
+	};
+flipCard(1);
+flipCard(2);
